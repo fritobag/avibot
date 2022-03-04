3e2742ec95bd5d7f0e0095e697f16d57ed518812
fritobag <69161300+fritobag@users.noreply.github.com>
2022-03-03 21:27:01 -0300

    ci: add test job

--- bot/avibot/__init__.py
+++ bot/avibot/__init__.py
@@ -1,1 +1,3 @@
+__version__ = "0.1.0"
+
 from .core import bot, checks, Cog, command, context, exceptions, group, logger
---
+++ bot/tests/test_avibot.py
@@ -0,0 +1,6 @@
+from avibot import __version__
+
+
+def test_version():
+    """Test version of the package."""
+    assert __version__ == "0.1.0"
